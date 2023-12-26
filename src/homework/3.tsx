707c2f46ef27c4a0c4b47e8284293069024631ac
Yurii <gapirov21@gmail.com>
2023-12-26 16:23:15 +0200

    task 3 done add ChangeEvent

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+// Ви створюєте компонент форми у React. Ви маєте поле введення, в якому ви хочете відстежити зміни. Для цього ви використовуєте обробник подій onChange. Ваше завдання – правильно типізувати подію, яка передається у цю функцію.
+
+import React, { useState, ChangeEvent } from "react";
 
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  return (
-    <input type="text" value={value} onChange={handleChange} />
-  );
+  return <input type="text" value={value} onChange={handleChange} />;
 }
